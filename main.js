361ab1c2afc9a8daf57e487e81146cbc1e60e6ac
miasnguyenx <miasnguyenx@gmail.com>
2024-06-11 10:05:31 +0700

    editing ...

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
 // khai bao library
-const { createServer } = require('node:http');
-
-if (process.env.NODE_ENV === 'development') {
-  console.log("hello")
+const { createServer } = require("node:http");
+const { EventEmitter } = require("node:events");
+// bien moi truong la node environment
+if (process.env.NODE_ENV === "development") {
+  console.log("hello");
 }
 
 // khai bao cac tham so can su dung de chay
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const port = 3000;
 
 // su dung library class
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+eventEmitter = new EventEmitter();
+eventEmitter.on('start', () => {
+  console.log('started');
 });
 
+eventEmitter.emit('start');
+
+
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
+});
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+
